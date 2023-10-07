6c854e2b5749fc7d6aca729de1af1ca19ee7c051
NicolasVZ86 <nvalenzuela2022@alu.uct.cl>
2023-10-07 20:39:47 -0300

    modificación index

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import  express  from "express"
 import {pool} from "./bd.js"
 import userRoutes from "./routes/usuarios.routes.js"
+const PORT = 3000
 
 const app = express()
 
@@ -9,4 +10,4 @@ app.use(express.json())
 app.use('/api',userRoutes)
 
 app.listen(3000)
-console.log("Server on port", 3000)
+console.log(`http://localhost:${PORT}/api/usuarios`)
