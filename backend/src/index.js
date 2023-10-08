f30268d93c9a1d52ee9d1e0237622e98daa42e61
Eduardo Escares <eduardoscrs60@gmail.com>
2023-10-08 19:39:27 -0300

    manejo de errores 1.1

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,5 +9,9 @@ const app = express()
 app.use(express.json())
 app.use('/api',userRoutes)
 
+app.use((req,res,next)=>{
+    res.status(404).json({message:"Endpoint not found"})
+})
+
 app.listen(3000)
 console.log(`http://localhost:${PORT}/api/usuarios`)
