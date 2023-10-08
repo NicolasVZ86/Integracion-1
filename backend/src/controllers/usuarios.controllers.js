392a7a7633068d5c8c653965ba22797522dfcadd
Eduardo Escares <eduardoscrs60@gmail.com>
2023-10-08 19:19:27 -0300

    Manejo de errores

--- backend/src/controllers/usuarios.controllers.js
+++ backend/src/controllers/usuarios.controllers.js
@@ -3,37 +3,64 @@ import { pool } from '../bd.js'
 
 
 export const getUsuarios = async (req, res) => {
-    // const {ID, Nombre, Apellido, Correo, Contraseña} = req.body
+    
+try {
     const [rows] = await pool.query('SELECT * FROM Usuario')
     res.json(rows)
+} catch (error) {
+    console.error('Error al obtener los usuarios:', error);
+    res.status(500).json({ message: "Error al obtener los usuarios" });
+}    
 }
+
 
 //const sql = 'SELECT * FROM usuarios'
 
 
 export const getUsuario = async (req, res) => {
-    const [rows] = await pool.query('SELECT * FROM Usuario WHERE ID = ?', [req.params.id])
-    if (rows.length <= 0) return res.status(404).json({ message: "El usuario no existe" })
-    res.json(rows[0])
-    
+
+    try {
+        const [rows] = await pool.query('SELECT * FROM Usuario WHERE ID = ?', [req.params.id])
+        if (rows.length <= 0) return res.status(404).json({ message: "El usuario no existe" })
+        res.json(rows[0])
+    } catch (error) {       
+        console.error('Error al obtener el usuario:', error);
+        res.status(500).json({ message: "Error al obtener el usuario" });
+        
+    }
+
 }
 
 export const createUsuarios = async (req, res) => {
-    const { ID, Nombre, Apellido, Correo, Contraseña } = req.body // Revisar que los nombres de los campos coincidan con los de la base de datos
-    const [rows] = await pool.query('INSERT INTO Usuario (ID, Nombre, Apellido, Correo, Contraseña) VALUES (?,?,?,?,?)', [ID, Nombre, Apellido, Correo, Contraseña]) // Asegúrate de que los nombres coincidan exactamente
-    console.log(req.body);
-    res.send({
-        ID: rows.insertId, Nombre, Apellido, Correo, Contraseña
-    })
+try {
+        const { ID, Nombre, Apellido, Correo, Contraseña } = req.body // Revisar que los nombres de los campos coincidan con los de la base de datos
+        const [rows] = await pool.query('INSERT INTO Usuario (ID, Nombre, Apellido, Correo, Contraseña) VALUES (?,?,?,?,?)', [ID, Nombre, Apellido, Correo, Contraseña]) // Asegúrate de que los nombres coincidan exactamente
+        console.log(req.body);
+        res.send({
+            ID: rows.insertId, Nombre, Apellido, Correo, Contraseña
+        })
+    } catch (error) {
+    console.error('Error al crear el usuario:', error);
+    res.status(500).json({ message: "Error al crear el usuario" });
+    
+    }
+
 }
 
 //const sql = 'INSERT INTO usuarios SET ?'
 
 
 export const deleteUsuarios = async (req, res) => {
-    const [rows] = await pool.query('DELETE FROM Usuario WHERE ID = ?', [req.params.id])
-    if (rows.affectedRows <= 0) return res.status(404).json({ message: "El usuario no existe" })
-    res.json({ message: "El usuario ha sido eliminado" })
+    try {
+        const [rows] = await pool.query('DELETE FROM Usuario WHERE ID = ?', [req.params.id])
+        if (rows.affectedRows <= 0) return res.status(404).json({ message: "El usuario no existe" })
+        res.json({ message: "El usuario ha sido eliminado" })
+    } catch (error) {
+        console.error('Error al eliminar el usuario:', error);
+        res.status(500).json({ message: "Error al eliminar el usuario" });
+        
+    }     
+
 }
 //const sql = 'DELETE FROM usuarios WHERE id = ?'
 
@@ -55,6 +82,7 @@ export const updateUsuarios = async (req, res) => {
         console.error('Error al actualizar el usuario:', error);
         res.status(500).json({ message: "Error al actualizar el usuario" });
     }
+
 }
 
 //const sql = 'UPDATE usuarios SET ? WHERE id = ?'
