7d294ead3eaeeb567f84d952c67af391f13fcb34
Eduardo Escares <eduardoscrs60@gmail.com>
2023-10-10 11:18:41 -0300

    Update usuarios.controllers.js

--- backend/src/controllers/usuarios.controllers.js
+++ backend/src/controllers/usuarios.controllers.js
@@ -32,20 +32,25 @@ export const getUsuario = async (req, res) => {
 }
 
 export const createUsuarios = async (req, res) => {
-try {
-        const { ID, Nombre, Apellido, Correo, Contraseña } = req.body // Revisar que los nombres de los campos coincidan con los de la base de datos
-        const [rows] = await pool.query('INSERT INTO Usuario (ID, Nombre, Apellido, Correo, Contraseña) VALUES (?,?,?,?,?)', [ID, Nombre, Apellido, Correo, Contraseña]) // Asegúrate de que los nombres coincidan exactamente
+    try {
+        const { ID, Nombre, Apellido, Correo, Contraseña } = req.body;
+        // Verificar si el usuario ya existe en la base de datos
+        const [existingUsers] = await pool.query('SELECT * FROM Usuario WHERE Correo = ?', [Correo]);
+        if (existingUsers.length > 0) {
+            return res.status(400).json({ message: "El usuario ya existe" });
+        }
+        // Si el usuario no existe, insertarlo en la base de datos
+        const [rows] = await pool.query('INSERT INTO Usuario (ID, Nombre, Apellido, Correo, Contraseña) VALUES (?,?,?,?,?)', [ID, Nombre, Apellido, Correo, Contraseña]);
         console.log(req.body);
         res.send({
             ID: rows.insertId, Nombre, Apellido, Correo, Contraseña
-        })
+        });
     } catch (error) {
-    console.error('Error al crear el usuario:', error);
-    res.status(500).json({ message: "Error al crear el usuario" });
-    
+        console.error('Error al crear el usuario:', error);
+        res.status(500).json({ message: "Error al crear el usuario" });
     }
-
 }
+
 
 //const sql = 'INSERT INTO usuarios SET ?'
 
@@ -69,21 +74,21 @@ export const updateUsuarios = async (req, res) => {
         const { id } = req.params;
         const { Nombre, Apellido, Correo, Contraseña } = req.body;
 
+        // Verificar si el usuario existe en la base de datos
+        const [existingUsers] = await pool.query('SELECT * FROM Usuario WHERE ID = ?', [id]);
+        if (existingUsers.length === 0) {
+            return res.status(404).json({ message: "El usuario no existe" });
+        }
 
+        // Realizar la actualización
         const [results] = await pool.query('UPDATE Usuario SET Nombre = IFNULL(?, Nombre), Apellido = IFNULL(?, Apellido), Correo = IFNULL(?, Correo), Contraseña = IFNULL(?, Contraseña) WHERE ID = ?', [Nombre, Apellido, Correo, Contraseña, id]);
 
-        if (results.affectedRows === 0) return res.status(404).json({ message: "El usuario no existe" });
-
-
         const [rows] = await pool.query('SELECT * FROM Usuario WHERE ID = ?', [id]);
-
         res.json(rows[0]);
     } catch (error) {
         console.error('Error al actualizar el usuario:', error);
         res.status(500).json({ message: "Error al actualizar el usuario" });
     }
-
 }
-
 //const sql = 'UPDATE usuarios SET ? WHERE id = ?'
 
