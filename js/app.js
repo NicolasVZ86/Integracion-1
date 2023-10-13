98ab43ac4cf078767ba8792e990b30c08943c0d9
valentinorr <106635273+valentinorr@users.noreply.github.com>
2023-10-13 14:39:48 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -4,43 +4,41 @@ const app = express();
 const cors = require('cors');
 const path = require('path');
 
-
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   database: 'bylanpdxjlmrw2ixoud8'
 });
+
 app.use(cors());
 
 // Ruta para servir el archivo index.html
 app.get('/', (req, res) => {
-    const indexPath = path.join(__dirname, 'public', 'index.html');
-    res.sendFile(indexPath);
-});
-app.get('/buscar-producto', (req, res) => {
-    // Obtiene el valor de búsqueda del parámetro de consulta
-    const valorBusqueda = req.query.busqueda;
-
-    // Simula resultados de búsqueda (aquí puedes usar un array de objetos que representen productos)
-    const resultados = [
-        { nombre: 'Producto 1', descripcion: 'Descripción del producto 1' },
-        { nombre: 'Producto 2', descripcion: 'Descripción del producto 2' }
-        // Agrega más productos simulados según sea necesario
-    ];
-
-    res.json(resultados);
+  const indexPath = path.join(__dirname, 'public', 'index.html');
+  res.sendFile(indexPath);
 });
 
-connection.connect(err => {
-  if (err) {
-    console.error('Error al conectar a la base de datos: ' + err.stack);
-    return;
-  }
-  console.log('Conexión a la base de datos establecida como ID ' + connection.threadId);
+// Ruta para simular productos en la base de datos
+app.get('/simular-productos', (req, res) => {
+  const productos = [
+    { Nombre: 'Producto 1', Descripcion: 'Descripción del producto 1' },
+    { Nombre: 'Producto 2', Descripcion: 'Descripción del producto 2' },
+  ];
+
+  // Insertar los productos simulados en la base de datos
+  connection.query('INSERT INTO Productos (Nombre, Descripcion) VALUES ?', [productos.map(p => [p.Nombre, p.Descripcion])], (error, results) => {
+    if (error) {
+      console.error('Error al simular productos: ' + error.stack);
+      res.status(500).json({ error: 'Error al simular productos.' });
+      return;
+    }
+    res.json({ message: 'Productos simulados correctamente.' });
+  });
 });
 
 app.use(express.json());
 
+// Ruta para buscar productos por valor de búsqueda
 app.get('/buscar-producto/:valorBusqueda', (req, res) => {
   const valorBusqueda = req.params.valorBusqueda.toLowerCase().trim();
   const query = `SELECT Nombre, Descripcion FROM Productos WHERE Descripcion LIKE '%${valorBusqueda}%'`;
