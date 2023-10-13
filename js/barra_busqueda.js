b65b47394e9b8d7136ecaf05b686c0f42f6e337a
valentinorr <106635273+valentinorr@users.noreply.github.com>
2023-10-13 14:38:56 -0300

    Update barra_busqueda.js

--- js/barra_busqueda.js
+++ js/barra_busqueda.js
@@ -1,27 +1,42 @@
 document.addEventListener('DOMContentLoaded', () => {
     const barraBusqueda = document.getElementById('barraBusqueda');
     const buscarBtn = document.getElementById('buscarBtn');
+    const resultadosLista = document.getElementById('resultadosLista');
+
+    const productos = [
+        { Nombre: 'Producto 1', Descripcion: 'Descripción del producto con número 123' },
+        { Nombre: 'Producto 2', Descripcion: 'Descripción del producto 456' }
+    ];
 
     buscarBtn.addEventListener('click', () => {
-        const valorBusqueda = barraBusqueda.value;
-         // Redirige a la página de resultados de búsqueda con el valor de búsqueda como parámetro de consulta
-        window.location.href = `/resultados.html?busqueda=${encodeURIComponent(valorBusqueda)}`;
-        fetch(`http://localhost:3001/buscar-producto/${valorBusqueda}`)
-            .then(response => response.json())
-            .then(resultados => {
-                console.log('Resultados de la búsqueda:', resultados);
-                // Llama a la función para mostrar los resultados
+        const valorBusqueda = barraBusqueda.value.trim().toLowerCase();
+
+        // Limpia resultados anteriores
+        resultadosLista.innerHTML = '';
+
+        if (valorBusqueda === '') {
+            const li = document.createElement('li');
+            li.textContent = 'No se encontraron resultados.';
+            resultadosLista.appendChild(li);
+        } else {
+            // Realizar la búsqueda solo si hay un término de búsqueda
+            const resultados = productos.filter(producto =>
+                producto.Nombre.toLowerCase().includes(valorBusqueda) ||
+                producto.Descripcion.toLowerCase().includes(valorBusqueda) ||
+                producto.Descripcion.includes(parseInt(valorBusqueda))
+            );
+
+            if (resultados.length === 0) {
+                const li = document.createElement('li');
+                li.textContent = 'No se encontraron resultados.';
+                resultadosLista.appendChild(li);
+            } else {
                 mostrarResultados(resultados);
-            })
-            .catch(error => console.error('Error al buscar productos:', error));
+            }
+        }
     });
 
     function mostrarResultados(resultados) {
-        // Código para mostrar los resultados en la interfaz de usuario
-        // Por ejemplo, podrías crear elementos de DOM para mostrar los resultados en una lista.
-        const resultadosLista = document.getElementById('resultadosLista');
-        resultadosLista.innerHTML = ''; // Limpiar resultados anteriores
-
         resultados.forEach(resultado => {
             const li = document.createElement('li');
             li.textContent = `Nombre: ${resultado.Nombre}, Descripción: ${resultado.Descripcion}`;
