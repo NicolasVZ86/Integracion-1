65df01ed01d8af3016c1a5abb3722c28d46332b4
BenjaminCantero <bcantero2023@alu.uct.cl>
2023-10-22 15:59:00 -0300

    actualizacion

--- fetch/editarUsuarios.js
+++ fetch/editarUsuarios.js
@@ -13,22 +13,34 @@ const dbConfig = {
 };
 const connection = mysql.createConnection(dbConfig);
 
-app.put('/editar/:id', (req, res) => {
-  const id = req.params.id;
-  const newData = req.body; // Los datos que deseas actualizar // nuevo dato
+document.addEventListener('DOMContentLoaded', () => {
+  const editarButton = document.getElementById('editarButton');
+  editarButton.addEventListener('click', () => {
+      const id = 1;  // Cambia el ID según tus necesidades
+      const nombre = document.getElementById('nombre').value;
+      const email = document.getElementById('email').value;
+      const password = document.getElementById('password').value;
 
-  connection.query('UPDATE Usuario SET ? WHERE ID = ?', [newData, id], (error, results) => {
-    if (error) {
-      console.error('Error al editar los datos: ', error);
-      res.status(500).json({ error: 'Error al editar los datos' });
-    } else {
-      res.json({ message: 'Datos actualizados con éxito' });
-    }
+      const newData = {
+          nombre,
+          email,
+          password
+      };
+
+      fetch(`/editar/${id}`, {
+          method: 'PUT',
+          headers: {
+              'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(newData)
+      })
+      .then(response => response.json())
+      .then(data => {
+          alert(data.message);
+      })
+      .catch(error => {
+          console.error('Error al editar los datos: ', error);
+          alert('Error al editar los datos');
+      });
   });
-});
-
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
-  console.log(`Servidor en ejecución en el puerto ${port}`);
 });
