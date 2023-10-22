5ea5d1c630ecb735de0684387bb0fdedef808bb4
BenjaminCantero <bcantero2023@alu.uct.cl>
2023-10-22 16:02:22 -0300

    act.

--- backend/src/controllers/envios.controllers.js
+++ backend/src/controllers/envios.controllers.js
@@ -1,17 +1,15 @@
-// Configura el middleware de bodyParser para analizar los datos del formulario
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// Ruta para servir archivos estáticos, como CSS y JavaScript
 app.use(express.static('public'));
 
-// Ruta para mostrar el formulario
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + "../envios.html");
 });
 
-// Ruta para manejar la solicitud POST del formulario
 app.post('/enviar', (req, res) => {
-  // Aquí puedes manejar la lógica para procesar los datos del formulario
+
   const nombre = req.body.nombre;
   const pais = req.body.Pais;
   const ciudad = req.body.Ciudad;
@@ -19,8 +17,7 @@ app.post('/enviar', (req, res) => {
   const telefono = req.body.telefono;
   const comentario = req.body.Comentario;
 
-  // Realiza alguna acción con los datos, como guardarlos en una base de datos
-  // En este ejemplo, solo los mostraremos en la consola
+
   console.log(`Nombre: ${nombre}`);
   console.log(`Pais: ${pais}`);
   console.log(`Ciudad: ${ciudad}`);
@@ -28,7 +25,6 @@ app.post('/enviar', (req, res) => {
   console.log(`Teléfono: ${telefono}`);
   console.log(`Comentario: ${comentario}`);
 
-  // Redirige a una página de confirmación o realiza alguna otra acción
   res.send('Pedido enviado con éxito');
 });
 
