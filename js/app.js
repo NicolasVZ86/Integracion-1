7cdea4d7f4bffe418b592a32c5634173ee57d19d
valentinorr <106635273+valentinorr@users.noreply.github.com>
2023-10-23 22:24:41 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -4,47 +4,27 @@ import cors from 'cors';
 import path from 'path';
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
+  connectionLimit: 10,
   host: 'localhost',
   user: 'root',
   database: 'bylanpdxjlmrw2ixoud8'
 });
 
-app.use(cors());
-
-// Ruta para servir el archivo index.html
 app.get('/', (req, res) => {
-  const indexPath = path.join(process.cwd(), 'public', 'index.html');
+  const indexPath = path.join(__dirname, 'public', 'index.html');
   res.sendFile(indexPath);
 });
 
-// Ruta para simular productos en la base de datos
-app.get('/simular-productos', (req, res) => {
-  const productos = [
-    { Nombre: 'Producto 1', Descripcion: 'Descripción del producto 1' },
-    { Nombre: 'Producto 2', Descripcion: 'Descripción del producto 2' },
-  ];
-
-  // Insertar los productos simulados en la base de datos
-  connection.query('INSERT INTO Productos (Nombre, Descripcion) VALUES ?', [productos.map(p => [p.Nombre, p.Descripcion])], (error, results) => {
-    if (error) {
-      console.error('Error al simular productos: ' + error.stack);
-      res.status(500).json({ error: 'Error al simular productos.' });
-      return;
-    }
-    res.json({ message: 'Productos simulados correctamente.' });
-  });
-});
-
-app.use(express.json());
-
-// Ruta para buscar productos por valor de búsqueda
 app.get('/buscar-producto/:valorBusqueda', (req, res) => {
   const valorBusqueda = req.params.valorBusqueda.toLowerCase().trim();
-  const query = `SELECT Nombre, Descripcion FROM Productos WHERE Descripcion LIKE '%${valorBusqueda}%'`;
+  const query = 'SELECT Nombre, Descripcion FROM Productos WHERE Descripcion LIKE ?';
+  const searchTerm = `%${valorBusqueda}%`;
 
-  connection.query(query, (error, results) => {
+  pool.query(query, [searchTerm], (error, results) => {
     if (error) {
       console.error('Error al realizar la consulta: ' + error.stack);
       res.status(500).json({ error: 'Error al buscar productos.' });
@@ -54,14 +34,20 @@ app.get('/buscar-producto/:valorBusqueda', (req, res) => {
   });
 });
 
-// Ruta para simular la suma de productos en el carrito de compra
 app.post('/simular-suma-productos', (req, res) => {
   const productosSeleccionados = req.body.productos; // Array de IDs de productos seleccionados
 
+  // Simulación de precios de productos (reemplaza esto con datos reales de la base de datos)
+  const preciosProductos = {
+    1: 10,
+    2: 20,
+    // ...otros productos y sus precios
+  };
+
   // Calcular la suma de precios de los productos seleccionados
   const precioTotal = productosSeleccionados.reduce((total, productoId) => {
-    if (productos[productoId]) {
-      return total + productos[productoId].precio;
+    if (preciosProductos[productoId]) {
+      return total + preciosProductos[productoId];
     }
     return total;
   }, 0);
@@ -69,8 +55,7 @@ app.post('/simular-suma-productos', (req, res) => {
   res.json({ precioTotal });
 });
 
-
-const PORT = 3001;
+const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Servidor iniciado en el puerto ${PORT}`);
 });
