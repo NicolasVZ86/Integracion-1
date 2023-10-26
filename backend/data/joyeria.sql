857109ffb5fd6458e80d098a0de047e08021e52f
BenjaminCantero <bcantero2023@alu.uct.cl>
2023-10-26 00:52:52 -0300

    es

--- backend/data/joyeria.sql
+++ backend/data/joyeria.sql
@@ -1,7 +1,6 @@
-CREATE DATABASE joyeria;
+CREATE DATABASE bylanpdxjlmrw2ixoud8;
 
-USE joyeria;
-
+USE bylanpdxjlmrw2ixoud8;
 
 CREATE TABLE Usuario (
     ID smallint unsigned auto_increment primary key,
