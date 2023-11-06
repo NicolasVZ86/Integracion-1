360212d7a4802e08e8c37d3526ca3622d35fe45a
Eduardo Escares <eduardoscrs60@gmail.com>
2023-11-06 19:10:21 -0300

    js valen

--- Frontend/html/js/barrabusq.js
+++ Frontend/html/js/barrabusq.js
@@ -1,95 +1,23 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const barraBusqueda = document.getElementById('barraBusqueda');
-    const buscarBtn = document.getElementById('buscarBtn');
+document.addEventListener("DOMContentLoaded", function () {
+    const searchButton = document.getElementById("buscarBtn");
+    const products = document.querySelectorAll(".product-item");
 
+    searchButton.addEventListener("click", function (event) {
+        event.preventDefault(); // Evita que el formulario se envíe
 
-    const productos = [
-        {
-            Nombre: 'anillo',
-            Imagen: '../img/images/productos/producto4.png',
-            Descripcion: 'anillo de plata hecho en italia'
-        },
-        {
-            Nombre: 'aro de oro ',
-            Imagen: '../img/images/productos/producto1.png',
-            Descripcion: 'aro de oro hecho en grecia,1kg'
-            
-        },
-        {
-            Nombre: 'collar de zafiro  ',
-            Imagen: '../img/images/productos/producto2.png',
-            Descripcion: 'collar de zafiro bañado en oro hecho en chile con minerales del espacio '
-            
-        },
-        {
-            Nombre: 'collar de perla  ',
-            Imagen: 'productos/producto3.png',
-            Descripcion: 'collar de perla hecho en chile con minerales del espacio de GOATlexis '
-            
-        },
-        {
-            Nombre: 'collar de carmesi  ',
-            Imagen: '../img/images/productos/productos4.png',
-            Descripcion: 'collar carmesi hecho en francia con minerales del espacio de GOATlexis '
-            
-        },
-        {
-            Nombre: 'collar de oro  ',
-            Imagen: '../img/images/productos/producto5.png',
-            Descripcion: 'collar bañado en oro hecho en chile con minerales del espacio de GOATlexis '
-            
-        },
-        {
-            Nombre: 'collar de plata  ',
-            Imagen: '../img/images/productos/producto6.png',
-            Descripcion: 'collar bañado en plata hecho en chile con minerales del espacio de GOATlexis '
-            
-        },
-        {
-            Nombre: 'collar de oro diamante  ',
-            Imagen: '../img/images/productos/producto7.jpg',
-            Descripcion: 'collar de oro con un diamante que cuelga hecho en argentina con minerales del espacio de GOATlexis '
-            
-        },
-        {
-            Nombre: 'collar de oro ligero  ',
-            Imagen: '../img/images/productos/producto8.jpg',
-            Descripcion: 'collar bañado en oro hecho en españa con minerales del espacio de GOATlexis '
-            
-        },
-        {
-            Nombre: 'anillo  ',
-            Imagen: '../img/images/productos/producto9.jpg',
-            Descripcion: 'anillo de oro con diamante hecho en italia con minerales del extraido de las islas caribeñas '
-            
-        }
-    ];
+        const searchBar = document.getElementById("barraBusqueda");
+        const searchTerm = searchBar.value.toLowerCase().trim(); // Elimina espacios al principio y al final
 
-    
-    
-    
-        buscarBtn.addEventListener('click', (event) => {
-            event.preventDefault();  // Evita que el formulario se envíe
-    
-            const valorBusqueda = barraBusqueda.value.trim().toLowerCase();
-    
-            if (valorBusqueda === '') {
-                alert('Por favor, ingrese un término de búsqueda.');
+        products.forEach(function (product) {
+            const productName = product.querySelector(".card-title").textContent.toLowerCase();
+            const productDescription = product.querySelector(".card-text").textContent.toLowerCase();
+            const productCategory = product.getAttribute("category").toLowerCase();
+
+            if (productName.includes(searchTerm) || productDescription.includes(searchTerm) || productCategory.includes(searchTerm) || searchTerm === "") {
+                product.style.display = "block";
             } else {
-                const resultadosFiltrados = productos.filter(producto =>
-                    producto.Nombre.toLowerCase().includes(valorBusqueda) ||
-                    producto.Descripcion.toLowerCase().includes(valorBusqueda) ||
-                    producto.Descripcion.includes(parseInt(valorBusqueda))
-                );
-    
-                if (resultadosFiltrados.length === 0) {
-                    alert('No se encontraron resultados.');
-                } else {
-                    const parametrosBusqueda = encodeURIComponent(JSON.stringify(resultadosFiltrados));
-                    window.location.href = 'resultados.html?resultados=' + parametrosBusqueda;
-                }
+                product.style.display = "none";
             }
         });
     });
-    
-
+});
