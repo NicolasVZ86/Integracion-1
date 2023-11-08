f5137f7859b970e0a22666a06e2cc96d2c17a463
Eduardo Escares <eduardoscrs60@gmail.com>
2023-11-08 14:22:30 -0300

    a

--- Frontend/html/js/validarSesion.js
+++
@@ -1,8 +0,0 @@
-console.log(window.location.pathname)
-if (window.location.pathname == "/Frontend/html/carrito.html") {
-    
-    console.log(localStorage.getItem('token'))
-    if (localStorage.getItem('token') === null) {
-        window.location.href = "/login.html"
-    }
-} else if (window.location.pathname == "/login.html") {}
