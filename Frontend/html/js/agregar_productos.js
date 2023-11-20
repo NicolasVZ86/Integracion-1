37be11db71ab931f1d9ba2929f34b572a8dad9c8
Eduardo Escares <eduardoscrs60@gmail.com>
2023-11-20 17:37:35 -0300

    Update agregar_productos.js

--- Frontend/html/js/agregar_productos.js
+++ Frontend/html/js/agregar_productos.js
@@ -1,37 +1,4 @@
-// TODO EL CODIGO COMENTADO ES BACKEND
-// const mysql = require('mysql');
 
-// // Configura los detalles de la conexión a la base de datos
-// const connection = mysql.createConnection({
-//   host: 'localhost',
-//   user: 'root',
-//   database: ' bylanpdxjlmrw2ixoud8'
-// });
-
-// // Conécta a la base de datos
-// connection.connect((err) => {
-//   if (err) {
-//     console.error('Error al conectar a la base de datos: ' + err.stack);
-//     return;
-//   }
-//   console.log('Conexión exitosa a la base de datos con el ID ' + connection.threadId);
-
-//   // Ejemplo
-//   connection.query('SELECT * FROM Productos', (error, results, fields) => {
-//     if (error) throw error;
-
-//     // Resultados de la consulta
-//     console.log('Productos:', results);
-//   });
-
-//   // Cierra la conexión después de realizar las consultas necesarias
-//   connection.end((err) => {
-//     if (err) {
-//       console.error('Error al cerrar la conexión: ' + err.stack);
-//       return;
-//     }
-//     console.log('Conexión cerrada.');
-//   });
 // });
 
 document.addEventListener("DOMContentLoaded", function() {
