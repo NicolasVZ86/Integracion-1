5819411f957c3e0312020fdbecac5317878ebcc1
valentinorr <106635273+valentinorr@users.noreply.github.com>
2023-11-21 21:20:46 -0300

    Add files via upload

---
+++ Frontend/js/loginDeCSS.js
@@ -0,0 +1,64 @@
+//Ejecutando funciones
+document.querySelector("#btn__iniciar-sesion").addEventListener("click", iniciarSesion)
+document.querySelector("#btn__registrarse").addEventListener("click", register)
+window.addEventListener("resize", anchoPage)
+
+//Declarando variables
+const formulario_login = document.querySelector("#formularioIniciarSesion")
+const formulario_register = document.querySelector("#formularioRegistrar")
+const contenedor_login_register = document.querySelector("#contenedorIniciarSesionRegistrar")
+const caja_trasera_login = document.querySelector("#cajaTraseraIniciarSesion")
+const caja_trasera_register = document.querySelector("#cajaTraseraRegistrar")
+
+    //FUNCIONES
+
+function anchoPage(){
+
+    if (window.innerWidth > 850){
+        caja_trasera_register.style.display = "block";
+        caja_trasera_login.style.display = "block";
+    }else{
+        caja_trasera_register.style.display = "block";
+        caja_trasera_register.style.opacity = "1";
+        caja_trasera_login.style.display = "none";
+        formulario_login.style.display = "block";
+        contenedor_login_register.style.left = "0px";
+        formulario_register.style.display = "none";   
+    }
+}
+
+anchoPage();
+
+
+    function iniciarSesion(){
+        if (window.innerWidth > 850){
+            formulario_login.style.display = "block";
+            contenedor_login_register.style.left = "10px";
+            formulario_register.style.display = "none";
+            caja_trasera_register.style.opacity = "1";
+            caja_trasera_login.style.opacity = "0";
+        }else{
+            formulario_login.style.display = "block";
+            contenedor_login_register.style.left = "0px";
+            formulario_register.style.display = "none";
+            caja_trasera_register.style.display = "block";
+            caja_trasera_login.style.display = "none";
+        }
+    }
+
+    function register(){
+        if (window.innerWidth > 850){
+            formulario_register.style.display = "block";
+            contenedor_login_register.style.left = "410px";
+            formulario_login.style.display = "none";
+            caja_trasera_register.style.opacity = "0";
+            caja_trasera_login.style.opacity = "1";
+        }else{
+            formulario_register.style.display = "block";
+            contenedor_login_register.style.left = "0px";
+            formulario_login.style.display = "none";
+            caja_trasera_register.style.display = "none";
+            caja_trasera_login.style.display = "block";
+            caja_trasera_login.style.opacity = "1";
+        }
+}
---
+++ Frontend/js/menu.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function() {
+<<<<<<< HEAD
+    const menuButton = document.querySelector(".menu button");
+    const menuNav = document.querySelector(".menu-nav");
+
+    menuButton.addEventListener("click", function() {
+        menuNav.classList.toggle("show-menu");
+    });
+});
+=======
+    const menuButton = document.querySelector(".menu button")
+    const menuNav = document.querySelector(".menu-nav")
+
+    menuButton.addEventListener("click", function() {
+        menuNav.classList.toggle("show-menu")
+    })
+})
+>>>>>>> nico
